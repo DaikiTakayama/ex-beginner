1ef3dccd935c6a0fb5485b8170b21613b6666099
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-05-20 14:29:30 +0900

    first commit

---
+++ src/main/java/jp/co/ex_beginner/ExBeginnerApplication.java
@@ -0,0 +1,13 @@
+package jp.co.ex_beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBeginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/ex_beginner/ExBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.ex_beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
